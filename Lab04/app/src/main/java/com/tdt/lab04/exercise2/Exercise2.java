115f5195068d5a813ec4ac5f84f7ab160f527af5
Ho Minh Chung <hmchung92@gmail.com>
2019-03-23 16:50:05 +0700

    Complete lab04 exercise

---
+++ Lab04/app/src/main/java/com/tdt/lab04/exercise2/Exercise2.java
@@ -0,0 +1,52 @@
+package com.tdt.lab04.exercise2;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+import android.widget.Toast;
+
+import com.tdt.lab04.R;
+
+import java.util.ArrayList;
+
+public class Exercise2 extends AppCompatActivity {
+    private ListView lvUsers;
+    private ArrayList<String> items;
+    private ItemsAdapter itemsAdapter;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_exercise_2);
+
+        initView();
+        initData();
+        handleListViewEvents();
+    }
+
+    private void handleListViewEvents() {
+        itemsAdapter = new ItemsAdapter(Exercise2.this, items);
+        lvUsers.setAdapter(itemsAdapter);
+        lvUsers.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                Toast.makeText(Exercise2.this, items.get(position), Toast.LENGTH_SHORT).show();
+            }
+        });
+    }
+
+    private void initData() {
+        items = new ArrayList<>();
+        for (int i = 1; i < 10; i++) {
+            String itemName = "item " + i;
+            items.add(itemName);
+        }
+    }
+
+    private void initView() {
+        lvUsers = (ListView) findViewById(R.id.lvUsers);
+    }
+}
---
+++ Lab04/app/src/main/java/com/tdt/lab04/exercise3/Exercise3.java
@@ -0,0 +1,22 @@
+package com.tdt.lab04.exercise3;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ListView;
+import android.widget.Toast;
+
+import com.tdt.lab04.R;
+import com.tdt.lab04.exercise2.ItemsAdapter;
+
+import java.util.ArrayList;
+
+public class Exercise3 extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_exercise_3);
+    }
+}
