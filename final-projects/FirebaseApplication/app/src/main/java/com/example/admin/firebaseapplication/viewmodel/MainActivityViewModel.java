7fd9ea762b3da753cc0288669cf845ed31bd017b
Admin <hmchung92@gmail.com>
2019-04-07 14:09:22 +0700

    Relocate folders

---
+++ final-projects/FirebaseApplication/app/src/main/java/com/example/admin/firebaseapplication/viewmodel/MainActivityViewModel.java
@@ -0,0 +1,32 @@
+package com.example.admin.firebaseapplication.viewmodel;
+
+import android.arch.lifecycle.ViewModel;
+
+import com.example.admin.firebaseapplication.Filters;
+
+public class MainActivityViewModel extends ViewModel {
+
+    private boolean mIsSigningIn;
+    private Filters mFilters;
+
+    public MainActivityViewModel() {
+        mIsSigningIn = false;
+        mFilters = Filters.getDefault();
+    }
+
+    public boolean getIsSigningIn() {
+        return mIsSigningIn;
+    }
+
+    public void setIsSigningIn(boolean mIsSigningIn) {
+        this.mIsSigningIn = mIsSigningIn;
+    }
+
+    public Filters getFilters() {
+        return mFilters;
+    }
+
+    public void setFilters(Filters mFilters) {
+        this.mFilters = mFilters;
+    }
+}
--- final-projects/FirebaseRestaurants/README.md
+++
@@ -1,1 +0,0 @@
-Firebase project: https://console.firebase.google.com/project/tdt-firestore-d67b0
